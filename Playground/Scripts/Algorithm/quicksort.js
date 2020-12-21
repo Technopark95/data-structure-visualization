a5cc0a349d0794a9292fc4672f9af9c99ad44d6e
Technopark95 <therain0605@gmail.com>
2020-12-21 11:10:42 +0530

    Update quicksort.js

--- Playground/Scripts/Algorithm/quicksort.js
+++ Playground/Scripts/Algorithm/quicksort.js
@@ -19,86 +19,57 @@ Copyright 2020 Anoop Singh, Graphical Structure
 
 */
 
-
-var sheet;
-
-$("#feed").fadeIn()
-$("#feed").css({"opacity" : "0%"})
-
 var logscreen = document.getElementById("log1");
 var outputscreen = document.getElementById("out1");
 
 
-function K( _k,  cellid )  {
+async function K( _k,  cellid )  {
 
     document.getElementById("kindex").style.transition = speed+"ms linear";
 
-    return new Promise (resolve => {
-    
+  
         let kpos = $(`#${cellid}item${_k}`).offset();
 
       
       $("#kindex").offset({top : (kpos.top)+100, left : (kpos.left)+45 });
 
       
-      
-      
-      setTimeout(function() {
-      
-      
-      resolve('')
-      
-      },speed)
-      
-      
-      })
+      await waitforme(speed)
+  
 
 
 }
 
-function I(_i , cellid="l" ) {
+async function I(_i , cellid="l" ) {
 
     document.getElementById("iindex").style.transition = speed+"ms linear";
 
 
-    return new Promise (resolve => {
     
         let ipos = $(`#${cellid}item${_i}`).offset();
         
     $("#iindex").offset({top : (ipos.top)+100, left : (ipos.left)+45 });
-    setTimeout(function() {
-    
-    
-    resolve('')
-    
-    },speed)
-    
-    
-    })
+   
+
+    await waitforme(speed)
     
 }
 
 
 
-function J(_j , cellid="r" ) {
+async function J(_j , cellid="r" ) {
 
 
     document.getElementById("jindex").style.transition = speed+"ms linear";
 
-    return new Promise (resolve => {
+  
     
         let jpos = $(`#${cellid}item${_j}`).offset();
         
         $("#jindex").offset({top : (jpos.top)+100, left : (jpos.left)+55 });
-    setTimeout(function() {
-    
-    
-    resolve('')
-    
-    },speed)
-    
-    
-    })
+  
+
+        await waitforme(speed)
     
 }
 
@@ -106,13 +77,13 @@ function J(_j , cellid="r" ) {
 
 
 
-function ij(_i ,_j  , cellid="a" , cellid1 = "a") {
+async function ij(_i ,_j  , cellid="a" , cellid1 = "a") {
 
 
-    document.getElementById("iindex").style.transition = speed+"ms linear";
-    document.getElementById("jindex").style.transition = speed+"ms linear";
+    ipointer.style.transition = speed+"ms linear";
+    jpointer.getElementById("jindex").style.transition = speed+"ms linear";
 
-    return new Promise (resolve => {
+   
     
         let ipos = $(`#${cellid}item${_i}`).offset();
         let jpos = $(`#${cellid1}item${_j}`).offset();
@@ -121,16 +92,9 @@ function ij(_i ,_j  , cellid="a" , cellid1 = "a") {
     $("#jindex").offset({top : (jpos.top)+100, left : (jpos.left)+55 });
     
     
+    await waitforme(speed)
     
-    setTimeout(function() {
-    
-    
-    resolve('')
-    
-    },speed)
-    
-    
-    })
+  
     
      }
 
@@ -139,7 +103,7 @@ function ij(_i ,_j  , cellid="a" , cellid1 = "a") {
 async function partition ( low,  high)  
 {  
     var pivot = storedarray[high]; // pivot  
-   await pivotchange(high);
+   await hilight("aitem"+high,"rgba(75,0,130, 0.842)");
     var qi = (low - 1); // Index of smaller element  
    // recurse(low , high)
   
@@ -151,7 +115,7 @@ async function partition ( low,  high)
         if (storedarray[qj] < pivot)  
         {  
           
-            await turnred(qj)
+            await hilight("aitem"+qj,"red")
             qi++; // increment index of smaller element
             await ij(qi,qj);
             await display('Incremenet i;  i='+qi)  
@@ -166,7 +130,7 @@ async function partition ( low,  high)
             await swapp(qi , qj);
      //       recurse(low , high)
         }
-        await turnnormal(qj)
+        await hilight("aitem"+qj,defaultcolor)
         }   
     }  
     var qtemp = storedarray[qi+1]
@@ -178,7 +142,7 @@ async function partition ( low,  high)
                 await swapp(qi+1 , high);
 
             }
-            await pivotnormal(high);
+            await hilight("aitem"+high,defaultcolor);
     return (qi + 1);  
 }  
   
@@ -223,7 +187,7 @@ async function qs(  low,  high)
 
 async function QuickSort()  {
 
-if($("#tab1").length == 0) stack(10);
+if( document.getElementById("tab1") == null) stack(10);
 
 await display('Calling QuickSort( '+ (0) + ' , ' + (length-1) + ' )' )
 await push('QuickSort( '+ (0) + ' , ' + (length-1) + ' )' )
@@ -231,170 +195,45 @@ await push('QuickSort( '+ (0) + ' , ' + (length-1) + ' )' )
 await cutoutarray(0,length-1)
 await qs(0 ,length-1)
 
-$("#iindex").text("i").hide()
-$("#jindex").text("j").hide()
+ipointer.innerHTML = "i"
+jpointer.innerHTML = "j";
+ipointer.style.display = "none";
+jpointer.style.display = "none";
 
 
-await normalize()
+await cutoutarray(0,length-1)
 
 }
 
 
 async function cutoutarray (start , end)  {
 
-    return new Promise( resolve => {
 
+for (let ie = 0; ie < storedarray.length; ie++) {
 
-$(".arrayd").css({"background-color" : "rgba(0,0,0,.2)" , transition: "400ms linear"})
-
-
-setTimeout(()=> {
-
-    for (var h = start ; h <= end ; ++h)  {
-
+    document.getElementById("aitem"+ie).style.transition = "400ms linear";
+    document.getElementById("aitem"+ie).style.backgroundColor = "rgba(0,0,0,.2)";
   
-        $(`#aitem${h}`).css({"background-color" : "rgba(0,0,0,.842)" , transition: "400ms linear"})
-  
-  
-  
+  }
+
+
+      for (let ie = start; ie <= end; ie++) {
+
+        document.getElementById("aitem"+ie).style.transition = "400ms linear";
+        document.getElementById("aitem"+ie).style.backgroundColor = "rgba(0,0,0,.842)";
+      
       }
-  
-      resolve('')
-
-
-},470)
-
-})
-
-
-}
-
-
-function sheet ()  {
-
- sheet = '<div id="Q" class="recursion-sheet" style="top:' + ($("#t1").position().top + 400) + 'px; left:'+  ($("#t1").position().left ) + 'px;">'
-
-$("body").append(sheet)
-$('#Q').draggable()
-
-Sheet = $("#Q")
-
-
-}
-
-
-
-
-function recurse(start , end)  {
-
-
-    var temparr = '<table style="border-collapse: collapse; margin-top:200px; text-align:center; transition-duration : 100ms;table-layout: fixed;" ><tr>'
+      
+      
+      
   
 
-    for (var h = start ; h <= end ; ++h)  {
 
-  
-      temparr += ' <td class="arrayd" style="text-align:center;">     <div  style="text-align:center; position:absolute; z-index:1">         <p  style="position:absolute; margin-top:50px; margin-left:70px; font-size:50%";>'+h+'</p>             <p style="position:absolute; margin-top:-30px;">'+ storedarray[h] +'</p>                        </div>  </td>';
-
-
-
-    }
-
-
-    temparr += '</tr></table>';
-
-
-
-$('#Q').append(temparr);
-
-}
-
-
-
-async function pivotchange(pivotindex)  {
-
- 
- $("#aitem"+pivotindex).css({ "background-color" : "rgba(75,0,130, 0.842)" , "transition" : speed+"ms linear"} , 3000 )
- if (stats == 1  ) await pauser();
-    return new Promise (resolve => {
-
-        setTimeout(() => {
-
-            resolve('resolved')
-        
-        
-        },speed+80)
-
-})
 
 
 }
 
 
-
-
-async function pivotnormal(pivotindex)  {
-
-  
-
- 
- $("#aitem"+pivotindex).css({ "background-color" : "rgba(0,0,0, 0.842)" , "transition" :speed+"ms linear"} , 3000 )
- if (stats == 1  ) await pauser();
-    return new Promise (resolve => {
-
-        setTimeout(() => {
-
-            resolve('resolved')
-        
-        
-        },speed+80)
-
-})
-
-}
-
-
-
-
-async function turnred(pivotindex)  {
-
-    $("#aitem"+pivotindex).css({ "background-color" : "rgba(255,0,0, 0.91)" , "transition" : speed+"ms linear"} , 3000 )
-    if (stats == 1  ) await pauser();
-    return new Promise (resolve => {
-
-        setTimeout(() => {
-
-            resolve('resolved')
-        
-        
-        },speed+80)
-
-})
-
- 
-
-
-}
-
-async function turnnormal(pivotindex)  {
-
-
-$("#aitem"+pivotindex).css({ "background-color" : "rgba(0,0,0, 0.842)" , "transition" : speed+"ms linear"} )
-
-if (stats == 1  ) await pauser();
-    return new Promise (resolve => {
-
-setTimeout(() => {
-
-    resolve('resolved')
-
-
-},speed+80)
-
-})
-
-
-}
 
 async function display (data , fin= 2000 , fout = 1000)  {
 
@@ -426,41 +265,6 @@ logscreen.scrollTop = logscreen.scrollHeight;
 
 
 }
-
-
-
-
-
-
-
- 
- // array([7,4,9,2,6,8,10,1,3,5])
-
- //array([5,24,45,65,89,92,104,147,190])
-
-   /*
- 
-
-
- arraynodes();
-
- doalign("1s" , 0);
- 
-
-
- 
-$("#t1").hide()
-
-
- doalign("4000ms" , 0);
- 
-  
- 
-*/
-
-//
-
-//array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,29,19,20,21,22,23,24,25,26,27,28,29,30,31]);
 
 
 
