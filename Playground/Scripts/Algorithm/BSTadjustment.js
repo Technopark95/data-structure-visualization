81d400ebfbe091e78ded9fb06e3238775b721970
Technopark95 <therain0605@gmail.com>
2021-01-15 22:39:31 +0530

    Update BSTadjustment.js

--- Playground/Scripts/Algorithm/BSTadjustment.js
+++ Playground/Scripts/Algorithm/BSTadjustment.js
@@ -14,8 +14,25 @@ async function adjustsubtree(node,val)  {
 
 
 
+}
+
+
+async function Pullup(node,val)  {
+
+
+    if (node == "null") {
+
+        return;
+    }
+    
+    document.getElementById(node).style.top = parseInt(document.getElementById(node).style.top)-85+"px"
+    Pullup(tree[node+"treeleft"],val)
+    Pullup(tree[node+"treeright"],val)
+
+
 
 }
+
 
 
 
@@ -87,7 +104,7 @@ async function  Shiftright(node)
 
 
    let slidermagnitude = referencecord +100;
-   let allmagnitude = slidermagnitude - referencecord;
+   let allmagnitude = slidermagnitude - rightmostcord;
 
 
    if ( rightmostcord - referencecord < 30 )  {
@@ -96,6 +113,103 @@ async function  Shiftright(node)
 
 
    }
+
+   await waitforme(speed)
+
+
+  
+    return; 
+} 
+
+
+
+
+
+async function  DelShiftright(node) 
+{ 
+
+    let current = tree[node+"treeleft"];
+    let starting = current;
+
+    if (current == "null") {
+        return;
+    }
+
+  
+    /* loop down to find the leftmost leaf */
+    while ( tree[`${current}treeright`] != "null")  {
+
+        current = tree[`${current}treeright`]; 
+
+    }
+
+   let rightmostcord = parseInt(document.getElementById(current).style.left)
+
+   let referencecord = parseInt(document.getElementById(node).style.left)
+
+
+
+   let slidermagnitude = referencecord -35;
+   let allmagnitude = slidermagnitude - rightmostcord;
+
+
+   if (referencecord -  rightmostcord > 50 )  {
+
+   await adjustsubtree(starting ,  allmagnitude)
+
+
+   }
+
+
+
+
+
+   await waitforme(speed)
+
+  
+    return; 
+} 
+
+
+
+
+
+async function  DelShiftleft(node) 
+{ 
+
+    let current = tree[node+"treeright"];
+    let starting = current;
+
+    if (current == "null") {
+        return;
+    }
+
+  
+    /* loop down to find the leftmost leaf */
+    while ( tree[`${current}treeleft`] != "null")  {
+
+        current = tree[`${current}treeleft`]; 
+
+    }
+
+   let rightmostcord = parseInt(document.getElementById(current).style.left)
+
+   let referencecord = parseInt(document.getElementById(node).style.left)
+
+
+
+   let slidermagnitude = referencecord +35;
+   let allmagnitude = rightmostcord - slidermagnitude;
+
+
+   if ( rightmostcord -referencecord > 50 )  {
+
+
+await adjustsubtree(starting ,  -allmagnitude)
+
+
+   }
+
 
    await waitforme(speed)
 
