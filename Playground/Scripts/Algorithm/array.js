b631778ec7520dab58d1d84b25bdaa130e0698de
Anoop Singh <therain0605@gmail.com>
2021-03-06 22:11:06 +0530

    Update array.js

--- Playground/Scripts/Algorithm/array.js
+++ Playground/Scripts/Algorithm/array.js
@@ -555,7 +555,7 @@ async function arraynodes()
   
      
 
-    newnode = '<div id="'+ii+'" style="left:0;top:0;"  class="dragg" > <div class="treenode" id="'+ ii+"treetop" +'" style="margin-left:35px;"></div>  <div class="treenode" id="'+ ii+"treeleft" +'" style="margin-left:18px; margin-top:70px;"></div>   <div class="treenode" id="'+ ii+"treeright" +'" style="margin-left:54px; margin-top:70px;"></div> <p  style="position:absolute;color:coral; font-size:70%; left:20px;" id="'+ ii+"bottom" +'">'+ii +'</p>  <p  style="color:white;margin-top:-2px;margin-left:-15px; font-size:50%;display:none;" id="'+ ii+"height" +'">'+"1" +'</p>  <p  id="'+ ii+"treeval" +'" class="t">'+storedarray[ii]+'</p>   </div>';
+    newnode = '<div id="'+ii+'" style="left:0;top:0;"  class="dragg" > <div class="treenode" id="'+ ii+"treetop" +'" style="margin-left:35px;"></div>  <div class="treenode" id="'+ ii+"treeleft" +'" style="margin-left:18px; margin-top:70px;"></div>   <div class="treenode" id="'+ ii+"treeright" +'" style="margin-left:54px; margin-top:70px;"></div> <p  style="position:absolute;color:coral; font-size:70%; left:20px;" id="'+ ii+"bottom" +'">'+ii +'</p>  <p  style="color:white;margin-top:-2px;margin-left:-15px; font-size:50%;display:none;" id="'+ ii+"height" +'">'+"1" +'</p> <p  style="color:white;margin-top:-2px;margin-left:-15px; font-size:50%;display:none;" id="'+ count+"height" +'">'+"1" +'</p>  <p  id="'+ ii+"treeval" +'" class="t">'+storedarray[ii]+'</p>   </div>';
 
    document.body.insertAdjacentHTML("afterbegin",newnode)
       
