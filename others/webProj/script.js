c7f3c5276960844130aac26319cd02d25061996e
Anoop Singh <therain0605@gmail.com>
2021-05-19 10:05:58 +0530

    Apply validations

--- others/webProj/script.js
+++ others/webProj/script.js
@@ -1,11 +1,36 @@
 
 let logoDiv  = document.getElementById("logoholder")
 
+let contactField  = document.getElementById("contactfield")
+
+
+contactField.onkeypress = function (evt)  {
+
+        let charCode = evt.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+        }
+        return true;
+    
+
+}
+
+
+const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 function clickSend()  {
 
+let allInputs = document.getElementsByClassName("inputs");
 
-    let startedElement =  document.getElementById("startedmessage");
+let name = allInputs[0].value;
+let email = allInputs[1].value;
+let contact = allInputs[2].value;
+let occupation = allInputs[3].value;
 
+
+
+
+    let startedElement =  document.getElementById("responsemessage");
     startedElement.innerText = "We will contact you soon."
 
 }
