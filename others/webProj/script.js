7f1f44da0a39bf077c3cc93b1079d1e17d01cfc1
Anoop Singh <therain0605@gmail.com>
2021-05-19 10:19:53 +0530

    validation Update

--- others/webProj/script.js
+++ others/webProj/script.js
@@ -1,22 +1,9 @@
 
 let logoDiv  = document.getElementById("logoholder")
-
 let contactField  = document.getElementById("contactfield")
-
-
-contactField.onkeypress = function (evt)  {
-
-        let charCode = evt.keyCode;
-        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-            return false;
-        }
-        return true;
-    
-
-}
-
-
 const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
 
 function clickSend()  {
 
@@ -28,10 +15,42 @@ let contact = allInputs[2].value;
 let occupation = allInputs[3].value;
 
 
+let isValidName = name.length > 0;
+let isValidEmail = emailRegEx.test(String(email).toLocaleLowerCase());
+let isValidContact = contact.length > 0;  
+let isValidOccupation = occupation.length > 0;  
+
+let responseText = "";
+let responseColor = "";
+
+if (isValidName && isValidEmail && isValidContact && isValidOccupation) {
+    responseText = "Thank you, We will contact you soon.";
+    responseColor = "lightgreen"
+}
+
+else {
+    responseText = "Check your input";
+    responseColor = "red"
+}
 
 
-    let startedElement =  document.getElementById("responsemessage");
-    startedElement.innerText = "We will contact you soon."
+let startedElement =  document.getElementById("responsemessage");
+
+
+    startedElement.innerText = responseText
+    startedElement.style.color = responseColor
+
+}
+
+
+contactField.onkeypress = function (evt)  {
+
+    let charCode = evt.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+
 
 }
 
