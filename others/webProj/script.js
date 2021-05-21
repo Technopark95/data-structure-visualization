c4e96d0932b8fc44f70c60ab98cf62880c6e1f7d
Anoop Singh <therain0605@gmail.com>
2021-05-21 12:08:15 +0530

    Update script.js

--- others/webProj/script.js
+++ others/webProj/script.js
@@ -2,7 +2,7 @@
 let logoDiv  = document.getElementById("logoholder")
 let contactField  = document.getElementById("contactfield")
 const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const nameRegEx = /^[A-Za-z]*$/;
+const nameRegEx =  /^[a-zA-Z\s]*$/;  
 
 
 function clickSend()  {
