4f1c0034596fd49444d247516c012c01616264c3
Anoop Singh <therain0605@gmail.com>
2021-05-21 16:07:01 +0530

    Enhance

---
+++ others/webProj/script/script.js
@@ -0,0 +1,173 @@
+
+let logoDiv = document.getElementById("logoholder")
+let contactField = document.getElementById("contactfield")
+const emailRegEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+function validateForm() {
+
+
+    let allInputs = document.getElementsByClassName("inputs");
+
+    let firstName = allInputs[0];
+    let lastName = allInputs[1];
+    let email = allInputs[2];
+    let contact = allInputs[3];
+    let occupation = allInputs[4];
+
+
+    let isValidFirstName = firstName.value.length > 0;
+
+    if (!isValidFirstName) {
+
+        firstName.style.border = "2px solid red";
+        firstName.focus();
+
+        setTimeout(() => {
+
+            firstName.style.border = "";
+
+        }, 1000)
+        return "First name";
+    }
+
+
+    let isValidLastName = lastName.value.length > 0;
+
+    if (!isValidLastName) {
+
+        lastName.style.border = "2px solid red";
+        lastName.focus();
+
+        setTimeout(() => {
+
+            lastName.style.border = "";
+
+        }, 1000)
+        return "Last name";
+    }
+
+    let isValidEmail = emailRegEx.test(String(email.value).toLowerCase());
+
+    if (!isValidEmail) {
+
+        email.style.border = "2px solid red";
+        email.focus();
+
+        setTimeout(() => {
+
+            email.style.border = "";
+
+        }, 1000)
+        return "email";
+    }
+
+
+    let isValidContact = contact.value.length > 0;
+
+
+    if (!isValidContact) {
+
+        contact.style.border = "2px solid red";
+        contact.focus();
+
+        setTimeout(() => {
+
+            contact.style.border = "";
+
+        }, 1000)
+        return "contact";
+    }
+
+
+
+    let isValidOccupation = occupation.value.length > 0;
+
+
+    if (!isValidOccupation) {
+
+        occupation.style.border = "2px solid red";
+        occupation.focus();
+
+        setTimeout(() => {
+
+            occupation.style.border = "";
+
+        }, 1000)
+        return "occupation";
+    }
+
+
+    return 0;
+
+
+
+}
+
+
+
+function showStatus(status) {
+
+
+    let responseText = "";
+    let responseColor = "";
+
+    if (status == 0) {
+        responseText = "Thank you, We will contact you soon.";
+        responseColor = "white"
+    }
+
+    else {
+        responseText = "Enter " + status + " correctly";
+        responseColor = "red"
+    }
+
+
+    let startedElement = document.getElementById("responsemessage");
+
+
+    startedElement.innerHTML = responseText
+    startedElement.style.color = responseColor
+    startedElement.style.display = "block"
+
+
+    setTimeout(() => {
+
+        startedElement.style.display = "none"
+
+    }, 3000)
+
+
+
+}
+
+
+
+function clickSend() {
+    
+    let status = validateForm();
+
+    showStatus(status);
+}
+
+
+window.onscroll = function () {
+
+
+    if (window.scrollY > 210) {
+        logoDiv.style.opacity = ".4";
+    }
+
+    if (window.scrollY <= 120) {
+        logoDiv.style.opacity = "0";
+    }
+
+
+}
+
+
+document.getElementById("notebtn").onclick = function () {
+    document.getElementById("noteholder").style.maxHeight = "140px";
+}
+
+
